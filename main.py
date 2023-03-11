5d7c3ec2bc64fd0aa71056ecd0e8aec619c7c89f
Drew <33384698+Ahendrix9624@users.noreply.github.com>
2023-03-11 14:25:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+USAGE - The code uses the PyAutoGUI library to move the mouse cursor randomly between 
+        the coordinates (600,200) and (700,600) with a duration of 0.5 seconds. 
+        It continues this action indefinitely using a while loop with a sleep time 
+        of 2 seconds between each mouse movement.
+        
+AUTHOR - https://github.com/Ahendrix9624/
+"""
 import pyautogui as pag
 import random
 import time
